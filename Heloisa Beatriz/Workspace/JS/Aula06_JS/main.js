b6b17678273dbde74bc272aa6f567af61011fd20
Helloisa22 <helloisa.betriz44@gmail.com>
2023-05-15 21:52:58 -0300

    Aula JS 06

--- Heloisa Beatriz/Workspace/JS/Aula06_JS/main.js
+++ Heloisa Beatriz/Workspace/JS/Aula06_JS/main.js
@@ -27,3 +27,29 @@ console.clear()
 const array1 = ["Maçã", "Kiwi", "Pera", "Tomate"]
 //                 0       1      2         3 
 console.log(array1.toString())
+console.clear()
+
+var atrasado = ["Matheus", "Abraao", "Erick", "Jhonnatan"]
+console.log(atrasado.join("*"))
+console.log(atrasado.length)
+let atrasado2 = atrasado.pop();
+console.log(atrasado2)
+console.log(atrasado)
+console.clear()
+let atrado2min = atrasado.push('Fequer')
+console.log(atrado2min)
+console.log(atrasado)
+console.clear()
+//shift()
+let atrasada3min = atrasado.shift();
+console.log(atrasada3min)
+console.log(atrasado)
+console.clear()
+let outroAtrasado = atrasado.unshift('Adrielly')
+console.log(outroAtrasado)
+console.log(atrasado)
+console.clear()
+
+delete atrasado[2]
+
+console.log(atrasado)

3af46494c552676a0164cff69da1f23d734f5cd7
HELOISA BEATRIZ DE OLIVEIRA COSTA <P0649973@portoseguro.com.br>
2023-05-28 22:54:55 -0300

    Exercicio DOM 3

---
+++ Heloisa Beatriz/Workspace/JS/DOM3/assets/exercicio.js
@@ -0,0 +1,12 @@
+let firstNumber = document.getElementById('first');
+let multiplicationTable = document.getElementById('blankSpace');
+
+const Calculate = () => {
+    let guardaValue = firstNumber.value;
+
+    for(i = 0; i <= 10; i++){
+        multiplicationTable.innerHTML += `<br> ${guardaValue} x ${[i]}  = ${guardaValue * [i]} <br>`
+    }
+
+    console.log(guardaValue)
+}
